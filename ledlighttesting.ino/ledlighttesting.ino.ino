d56ddc847aa5eae8be4421441be11204be8915ee
EdwinSuubi <edwinsuubi@gmail.com>
2019-06-05 16:43:02 +0300

    [Bwowe] I changed the pin for red from 6 to 7

---
+++ ledlighttesting.ino/ledlighttesting.ino.ino
@@ -0,0 +1,24 @@
+int green = 11;
+int red  = 6;
+int yellow =4;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT); 
+  pinMode(red, OUTPUT); 
+   
+}
+
+void loop() {
+  digitalWrite(green, HIGH);
+  delay(1000);
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  delay(1000);
+  digitalWrite(red, LOW);
+  // put your main code here, to run repeatedly:
+
+}
